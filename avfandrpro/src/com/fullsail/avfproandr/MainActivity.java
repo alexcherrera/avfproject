adbd004ea8ec5a4538fc48e32700f3a62adea0e7
Alexander Herrera <alexcherrera@fullsail.edu>
2012-09-27 20:35:14 -0400

    ready to submit files

--- avfandrpro/src/com/fullsail/avfproandr/MainActivity.java
+++ avfandrpro/src/com/fullsail/avfproandr/MainActivity.java
@@ -1,13 +1,13 @@
 package com.fullsail.avfproandr;
 
 import android.os.Bundle;
-import org.apache.cordova.*;
+import org.apache.cordova.DroidGap;
 
 public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///Users/alexcherrera/Desktop/FullSail/Month%209/avfproject/avfandrpro/assets/www/index.html");  
+        super.loadUrl("file:///_assets/www/index.html");  
     }
 }
